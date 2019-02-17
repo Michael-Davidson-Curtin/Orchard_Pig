0f932a36b52e288ba1ece7106eddc725ec9b5f59
Michael Davidson <s3353147@student.rmit.edu.au>
2019-02-17 15:29:16 +1100

    Staurday 17th Work- Nav bar and boostrap card

--- global.js
+++ global.js
@@ -1,43 +1,97 @@
 $(document).ready(function(){
-   
-    
+    var id=0;
+    //console.log("you clicked the Bottled button");
+    //get from Json the category information for bottled drinks and assemble here
+    $.getJSON("API/get_drink_categories.php?id=0",function(json){
+        //console.log(json);
+        $('#drink_category_title').html(json[id].Short_Description); 
+        $('#drink_category_text').html(json[id].Long_Description);
+    });
+    $.getJSON("API/get_drinks_by_category.php?id="+id,function(drinks){
+        //console.log(drinks);
+        $('#drinks-links').empty();
+        for (i=0;i<drinks.length;i++)
+        {
+            //$('#drinks-links').html("<a href='#' class='card-link' id='drink"+i+"'>"+drinks[i].name+"</a>");           
+            $('#drinks-links').append("<a href='#' class='card-link' id="+drinks[i].name+">"+drinks[i].name+"</a>");   
+        }
+ });
+  
 //When the doc loads have bottled as visible
 $('#bottled').on('click',function(){
     var id=0;
-    console.log("you clicked the Bottled button");
+    //console.log("you clicked the Bottled button");
     //get from Json the category information for bottled drinks and assemble here
     $.getJSON("API/get_drink_categories.php?id=0",function(json){
-        console.log(json);
-        $('#heading').replaceWith("<h2>"+json[id].Short_Description+"</h2>"); 
-        $('#main-text').replaceWith("<div id='main-text'><p>"+json[id].Long_Description+"<p></div>");
+        //console.log(json);
+        $('#drink_category_title').html(json[id].Short_Description); 
+        $('#drink_category_text').html(json[id].Long_Description);
     });
     $.getJSON("API/get_drinks_by_category.php?id="+id,function(drinks){
-        console.log(drinks);
+        //console.log(drinks);
+        $('#drinks-links').empty();
         for (i=0;i<drinks.length;i++)
         {
-            $('#drink_links').append("<div class='col-3'><p id="+drinks[i].name+">"+drinks[i].name+"</p></div>");    
+            //$('#drinks-links').html("<a href='#' class='card-link' id='drink"+i+"'>"+drinks[i].name+"</a>");           
+            $('#drinks-links').append("<a href='#' class='card-link' id="+drinks[i].name+">"+drinks[i].name+"</a>");   
         }
-        //go throught the list of drinks returned and for each of them create a 
-        //<div class="col-3" style="color:#2e4a87;"><p>Reveller</p></div>
-    });
-    
-    
-   
-   
+ });
 });
+
 
 $('#draught').on('click',function(){
-    console.log("you clicked the Draught button");
-    //get from Json the category information for Draught drinks and assemble here
- 
+        //console.log("you clicked the Draught button");
+        //get from Json the category information for Draught drinks and assemble here
+        var id=1;
+        //console.log("you clicked the Bottled button");
+        //get from Json the category information for bottled drinks and assemble here
+        $.getJSON("API/get_drink_categories.php?id=0",function(json){
+            //console.log(json);
+            $('#drink_category_title').html(json[id].Short_Description); 
+            $('#drink_category_text').html(json[id].Long_Description);
+            
+        });
+        $.getJSON("API/get_drinks_by_category.php?id="+id,function(drinks){
+            //console.log(drinks);
+            $('#drinks-links').empty();
+            for (i=0;i<drinks.length;i++)
+            {
+                $('#drinks-links').append("<a href='#' class='card-link' id="+drinks[i].name+">"+drinks[i].name+"</a>");   
+                //$('#drink_category_text').after("<a href='#' class='card-link'>"+drinks[i].name+"</a>");
+                //$('#drink_links').append("<div class='col-sm'><p id="+drinks[i].name+">"+drinks[i].name+"</p></div>");    
+            }
+    
     
 });
-
-$('#juicy').on('click',function(){
-    console.log("you clicked the Juicy button");
-    //get from Json the category information for Juicy drinks and assemble here
-   
 });
 
 
-});//end page
+$('#juicy').on('click',function(){
+    //console.log("you clicked the Juicy button");
+    //get from Json the category information for Juicy drinks and assemble here
+    var id=2;
+    //console.log("you clicked the Bottled button");
+    //get from Json the category information for bottled drinks and assemble here
+    $.getJSON("API/get_drink_categories.php?id=0",function(json){
+       // console.log(json);
+        $('#drink_category_title').html(json[id].Short_Description); 
+        $('#drink_category_text').html(json[id].Long_Description);
+       
+    });
+    $.getJSON("API/get_drinks_by_category.php?id="+id,function(drinks){
+       // console.log(drinks);
+        $('#drinks-links').empty();
+        //need to replace this when new content is made
+        
+        for (i=0;i<drinks.length;i++)
+        {
+            $('#drinks-links').append("<a href='#' class='card-link' id=juicy"+drinks[i].id+">"+drinks[i].name+"</a>");   
+            //$('#drink_category_text').after("<a href='#' class='card-link'>"+drinks[i].name+"</a>");
+            //$('#drink_links').append("<div class='col-4'><p id="+drinks[i].name+">"+drinks[i].name+"</p></div>");    
+        }
+
+});
+});
+});//end page 
+
+
--- main.php
+++ main.php
@@ -2,24 +2,23 @@
 <html lang="en">
   <head>
     <meta charset="utf-8"> 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS\styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+   <link rel="stylesheet" href="CSS\styles.css">
     <title>Orchard Pig - The Pig Sty</title>
   </head>
-  <body class="cursor">
+<body class="cursor">
   
 <!-- Navigation Bar -->
 
-<nav class="navbar navbar-expand-lg bg-info navbar-dark" >
-
+<nav class="navbar navbar-expand-lg bg-info navbar-dark " >
   <a class="navbar-brand" href="main.php" id="logo_text">Orchard Pig</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+  <div class="navbar-collapse justify-content-md-center collapse" id="collapsibleNavbar">
   <ul class="navbar-nav" >
-    <li class="nav-item active">
-      <a class="nav-link" href="#drinks" id="our_drinks" >Our Drinks</a>
+    <li class="nav-item ">
+      <a class="nav-link" href="#bottled" id="our_drinks" >Our Drinks</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#about" id="about_us">About Us</a>
@@ -31,38 +30,63 @@
 </div>
 </nav>
 
+<!--Nav 2-->
+
 
 <!--Hero Section -->
+<div class="jumbotron.jumbotron-fluid" id="Hero_Background___Home"></div>
 
-  <main class="container-fluid">
-      <div class="container-fluid" id="Hero_Background___Home"></div>
 <!-- Green Patch-->
-      <div class="container-fluid" id="green_patch">
-          <div class="row">
-            <!-- Bottled Sign Post -->
-            <div class="col-4" >
-                <img src="img/Bottled Sign Post.png" class="signpost" id="bottled">
-            </div>
-            <!-- Draught Sign Post -->
-            <div class="col-4" >
-              <img src="img/Draught Sign Post.png" class="signpost" id="draught">
-            </div>
-            <!-- Juicy Sign Post -->
-            <div class="col-4" >
-             <img src="img/Juicy Sign Post.png" class="signpost" id="juicy">
-            </div>
-          </div>
-      </div>
+<div class="container-fluid" id="green_patch">
+  <div class="row" >
+  <!-- Bottled Sign Post -->
+    <div class="col-sm" >
+      <img src="img/Bottled Sign Post.png" href="#drinks"class="signpost" id="bottled">
+    </div>
+  <!-- Draught Sign Post -->
+    <div class="col-sm" >
+        <img src="img/Draught Sign Post.png" class="signpost" id="draught">
+    </div>
+  <!-- Juicy Sign Post -->
+    <div class="col-sm" >
+      <img src="img/Juicy Sign Post.png" href="#drinks" class="signpost" id="juicy">
+    </div>
+  </div>
+</div>
+<!-- Green Patch End-->
 
-      <!-- Drinks Information Panel Container -->
-      <div class="container-fluid" id="drinks">
-        <!-- Drinks Information Panel Heading --> 
-        <h1>Our Drinks...</h1> 
-          <!-- Drinks Information Panel  -->
-         <div class="container" >
-         <!--Blank Space -->    
+<!-- Drinks Information Panel Container -->
+<div class="container-fluid" id="drinks">
+<!-- Drinks Information Panel Heading -->
+  <h1>Our Drinks...</h1> 
+  <!-- Drinks Information Panel  -->
+    <div class="container" >
+    <!-- want to trial a horizontal card -->
+    <div class="card mb-3"  id="drink-card">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+           <img src="img\bottles-main.png" class="card-img" alt="...">
+        </div><!-- col-md-4 - End -->
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title" id="drink_category_title"></h5>
+          <p class="card-text" id="drink_category_text"></p>
+        <!-- Need links to drinks -->
+        <div id="drinks-links">
+        
+        </div>
+       
+        <!-- Drink Links End -->
+        </div><!-- card-body - End -->
+      </div><!-- col-md-8 - End -->
+      </div><!-- row - End -->
+    </div> <!-- Drink-card - End -->
+    </div><!-- Container - End -->
+</div><!--Container-fluid - End -->
+        
+         <!--Blank Space 
          <div class="col-1"></div>
-        <!-- Information -->
+        <!-- Information 
              <div id="bottled_info">
              <div class="col-12 " id="info">
                      <div class="info_image"></div>
@@ -73,12 +97,12 @@
                              </div>
                           <div class="row" id="drink_links">
                             
-                              <div class="col-3" style="color:#2e4a87;"><p id="test">Reveller</p></div>
+                              
                             <!--
                               <div class="col-3" style="color:#f98847;"><p>Charmer</p></div>
                             <div class="col-3" style="color:#82bb61;"><p>Truffler</p></div>
                             <div class="col-3" style="color:#9a667d;"><p>Hog Father</p></div>
--->
+
                           </div>
                          </article>
                         </div>
@@ -88,9 +112,9 @@
      
              <div class="col-1"></div>
              
-         </div>
+         </div> -->
           
-      </div>
+       
       <div class="container-fluid" id="about">
       
           <h1>Our History...</h1>
@@ -148,7 +172,7 @@
       </div>
   </main>    
   
-<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
